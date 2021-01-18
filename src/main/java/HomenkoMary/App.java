21fa322f592e3b876d8755bb1565c34333f6d087
HomenkoMary <61244966+HomenkoMary@users.noreply.github.com>
2021-01-18 17:40:17 +0400

    init

---
+++ src/main/java/HomenkoMary/App.java
@@ -0,0 +1,13 @@
+package HomenkoMary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
